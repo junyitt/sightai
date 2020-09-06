95bf311b66fa191157b0dcdc7b5f40147bd05b77
junyitt <junyitt1995@gmail.com>
2020-09-06 15:26:37 +0800

    new output dir

--- src/load_video.py
+++ src/load_video.py
@@ -1,74 +1,34 @@
 import cv2
-from sightai.module import SightAI
-import gtts
-from playsound import playsound
-import pyttsx3
-import threading
 import numpy as np
+from sightai.module import SightAI
 
-
-
-engine = pyttsx3.init()
-voices = engine.getProperty("voices")
 
 S = SightAI(use_cuda = True)
 
-def play_sound(text):
-    engine.setProperty("voice", voices[1].id)
-    engine.say(text)
-    engine.runAndWait()
-
-def run_infer(imgfile, j, plot=False):
-    msg, df = S.inference(imgfile, plot = plot, j = j)
-    print(df)
-    for text in msg:
-        engine.setProperty("voice", voices[1].id)
-        engine.say(text)
-        engine.runAndWait()
-
-# vidcap = cv2.VideoCapture('media/MALAYSIA Kuala Lumpur 15 -Walk- from KL Sentral Monorail Sta. to Muzium Negara MRT Sta.mp4')
-# vidcap = cv2.VideoCapture('media/London Underground to Waterloo incl. South Bank Walking Tour.mp4')
 filename = "two_way"
 vidcap = cv2.VideoCapture('media/{}.mp4'.format(filename))
 frame_width = int(vidcap.get(3))
 frame_height = int(vidcap.get(4))
 
-# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-# out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
-# out = cv2.VideoWriter('outpy2.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
-
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-# out1 = cv2.VideoWriter('out1.avi', fourcc, 20.0, (frame_width,frame_height))
-# out2 = cv2.VideoWriter('out2.avi', fourcc, 20.0, (frame_width,frame_height))
-# out3 = cv2.VideoWriter('out3.avi', fourcc, 20.0, (frame_width,frame_height))
-out4 = cv2.VideoWriter('out_{}.avi'.format(filename), fourcc, 30.0, (frame_width*2,frame_height*2))
-
+out4 = cv2.VideoWriter('output_video/out_{}.avi'.format(filename), fourcc, 30.0, (frame_width*2,frame_height*2))
 
 if (vidcap.isOpened()== False): 
     print("Error opening video stream or file")
 
-max_count = None # no limit
+max_count = 20 # no limit
 success,image = vidcap.read()
 count = 0
 play = 0
 while(vidcap.isOpened()):
     success,image = vidcap.read()
     
-    # if count % 15 == 0 and play % 45 == 0:
-    #     # imgfile = "./frame/frame{}.jpg".format(count)
-    #     imgfile = "./frame/frame{}.jpg".format(count)
-    #     cv2.imwrite(imgfile, image)     # save frame as JPEG file  
-    #     run_infer(imgfile, count, plot = True)    
-    #     # t = threading.Thread(target=run_infer, args=(imgfile,))
-    #     # t.start()  
-
-
     if success:
-        imgfile = "./frame/fr{}.jpg".format(count)
-        dimgfile = "./frame/dmap_{}.jpg".format(count)
-        dimgfile2 = "./frame/dmap2_{}.jpg".format(count)
-        dimgfile3 = "./frame/dmap3_{}.jpg".format(count)
-        dimgfile4 = "./frame/dmap4_{}.jpg".format(count)
+        imgfile = "./output/fr{}.jpg".format(count)
+        dimgfile = "./output/dmap_{}.jpg".format(count)
+        dimgfile2 = "./output/dmap2_{}.jpg".format(count)
+        dimgfile3 = "./output/dmap3_{}.jpg".format(count)
+        dimgfile4 = "./output/dmap4_{}.jpg".format(count)
         cv2.imwrite(imgfile, image)
 
         dmap, img, grid_dmap, sensor_gimg = S.inference(imgfile, plot = False, j = count)  
